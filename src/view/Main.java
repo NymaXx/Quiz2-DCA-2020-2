e5fb519c41fdcf6e653af77aa9829fa9540127da
NymaXx <53951684+NymaXx@users.noreply.github.com>
2020-11-27 18:01:08 -0500

    Creacion de las clases y paquetes

---
+++ src/view/Main.java
@@ -0,0 +1,33 @@
+package view;
+
+import processing.core.PApplet;
+
+public class Main extends PApplet{
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		PApplet.main(Main.class.getName());
+	}
+	
+	
+	public void settings() {
+		size(800,500);
+	}
+	
+	public void setup() {
+		
+	}
+	
+	public void draw() {
+		background(0);
+	}
+	
+	public void mousePressed() {
+		
+	}
+	
+	public void keyPressed() {
+		
+	}
+
+}
